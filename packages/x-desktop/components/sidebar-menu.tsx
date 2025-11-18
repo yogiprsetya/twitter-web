d21f22ce9661c390465e1342d62435fdb9e0d2e7
Yogi Prasetya <yogi.prasetya@digitaltelkom.id>
2025-11-18 20:55:04 +0700

    profile bton

---
+++ packages/x-desktop/components/sidebar-menu.tsx
@@ -0,0 +1,89 @@
+import {
+  HomeIconDefault,
+  HomeIconSelected,
+  ExploreIconDefault,
+  ExploreIconSelected,
+  NotificationsIconDefault,
+  NotificationsIconSelected,
+  MessagesIconDefault,
+  MessagesIconSelected,
+  BookmarksIconDefault,
+  BookmarksIconSelected,
+  ListsIconDefault,
+  ListsIconSelected,
+  ProfileIconDefault,
+  ProfileIconSelected,
+  MoreIcon,
+} from '@twitter-web/ui';
+
+const NAV = [
+  {
+    label: 'Home',
+    icon: {
+      default: <HomeIconDefault />,
+      selected: <HomeIconSelected />,
+    },
+  },
+  {
+    label: 'Explore',
+    icon: {
+      default: <ExploreIconDefault />,
+      selected: <ExploreIconSelected />,
+    },
+  },
+  {
+    label: 'Notification',
+    icon: {
+      default: <NotificationsIconDefault />,
+      selected: <NotificationsIconSelected />,
+    },
+  },
+  {
+    label: 'Messages',
+    icon: {
+      default: <MessagesIconDefault />,
+      selected: <MessagesIconSelected />,
+    },
+  },
+  {
+    label: 'Bookmarks',
+    icon: {
+      default: <BookmarksIconDefault />,
+      selected: <BookmarksIconSelected />,
+    },
+  },
+  {
+    label: 'Lists',
+    icon: {
+      default: <ListsIconDefault />,
+      selected: <ListsIconSelected />,
+    },
+  },
+  {
+    label: 'Profile',
+    icon: {
+      default: <ProfileIconDefault />,
+      selected: <ProfileIconSelected />,
+    },
+  },
+];
+
+export const SidebarMenu = () => {
+  return (
+    <>
+      {NAV.map((v) => (
+        <a
+          key={v.label}
+          className="[&_svg]:size-6 flex items-center gap-5 h-14 px-3"
+        >
+          {v.icon.default} <span className="font-bold text-lg">{v.label}</span>
+        </a>
+      ))}
+
+      <button type="button" className="flex items-center gap-5 h-14 px-3 mb-6">
+        <MoreIcon className="size-6 " />{' '}
+        <span className="font-bold text-lg">More</span>
+      </button>
+    </>
+  );
+};
--- packages/x-desktop/components/sidebar.tsx
+++ packages/x-desktop/components/sidebar.tsx
@@ -1,102 +1,43 @@
 import {
-  HomeIconDefault,
-  HomeIconSelected,
-  ExploreIconDefault,
-  ExploreIconSelected,
-  NotificationsIconDefault,
-  NotificationsIconSelected,
-  MessagesIconDefault,
-  MessagesIconSelected,
-  BookmarksIconDefault,
-  BookmarksIconSelected,
-  ListsIconDefault,
-  ListsIconSelected,
-  ProfileIconDefault,
-  ProfileIconSelected,
-  MoreIcon,
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
   Button,
+  EllipsesIcon,
 } from '@twitter-web/ui';
 import LogoX from '~/public/logo-x.svg';
-
-const NAV = [
-  {
-    label: 'Home',
-    icon: {
-      default: <HomeIconDefault />,
-      selected: <HomeIconSelected />,
-    },
-  },
-  {
-    label: 'Explore',
-    icon: {
-      default: <ExploreIconDefault />,
-      selected: <ExploreIconSelected />,
-    },
-  },
-  {
-    label: 'Notification',
-    icon: {
-      default: <NotificationsIconDefault />,
-      selected: <NotificationsIconSelected />,
-    },
-  },
-  {
-    label: 'Messages',
-    icon: {
-      default: <MessagesIconDefault />,
-      selected: <MessagesIconSelected />,
-    },
-  },
-  {
-    label: 'Bookmarks',
-    icon: {
-      default: <BookmarksIconDefault />,
-      selected: <BookmarksIconSelected />,
-    },
-  },
-  {
-    label: 'Lists',
-    icon: {
-      default: <ListsIconDefault />,
-      selected: <ListsIconSelected />,
-    },
-  },
-  {
-    label: 'Profile',
-    icon: {
-      default: <ProfileIconDefault />,
-      selected: <ProfileIconSelected />,
-    },
-  },
-];
+import { SidebarMenu } from './sidebar-menu';
 
 export const Sidebar = () => {
   return (
-    <aside style={{ width: 275 }} className="h-full pr-8">
+    <aside style={{ width: 275 }} className="h-full pr-8 relative">
       <div className="overflow-hidden size-[52px] rounded-full flex items-center justify-center">
         <LogoX className="fill-primary-blue dark:fill-white size-8" />
       </div>
 
-      <nav className="mt-1 mb-6">
-        {NAV.map((v) => (
-          <a
-            key={v.label}
-            className="[&_svg]:size-6 flex items-center gap-5 h-14 px-3"
-          >
-            {v.icon.default}{' '}
-            <span className="font-bold text-lg">{v.label}</span>
-          </a>
-        ))}
+      <nav className="mt-1 mb-10">
+        <SidebarMenu />
 
-        <button type="button" className="flex items-center gap-5 h-14 px-3">
-          <MoreIcon className="size-6 " />{' '}
-          <span className="font-bold text-lg">More</span>
-        </button>
+        <Button size="big" className="w-full">
+          Tweet
+        </Button>
       </nav>
 
-      <Button size="big" className="w-full">
-        Tweet
-      </Button>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Avatar className="size-12">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>YP</AvatarFallback>
+          </Avatar>
+
+          <div className="flex flex-col justify-center">
+            <p className="font-bold leading-3">Yogi Prasetya</p>
+            <p className="font-medium text-dark-6">@yogiprsty</p>
+          </div>
+        </div>
+
+        <EllipsesIcon className="size-8" />
+      </div>
     </aside>
   );
 };
